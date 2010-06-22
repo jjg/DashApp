39f025c347608db9318427db22de21ff591064df
Jason Gullickson <jason.gullickson@gmail.com>
2010-06-22 00:13:22 -0500

    Added GPL info

--- Classes/GaugeClusterViewController.m
+++ Classes/GaugeClusterViewController.m
@@ -3,8 +3,27 @@
 //  GaugeCluster
 //
 //  Created by Jason Gullickson on 3/15/10.
-//  Copyright the second society 2010. All rights reserved.
+//  Copyright Jason Gullickson 2010.
 //
+
+/*
+ 
+ This file is part of DashApp.
+ 
+ DashApp is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ DashApp is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with DashApp.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
 
 #import "GaugeClusterViewController.h"
 #import "WidgetView.h"
